df8a3eadcf3b664200bead24f00eb76aba2a8a51
iamgqr <1824728600@qq.com>
2019-06-21 15:45:00 +0800

    Added two charts

--- author.php
+++ author.php
@@ -13,8 +13,11 @@
 
 <body>
 	<h1>Author Page</h1>
-	<div id="paperchart" style="width: 600px;height:400px;"></div>
+	<div id="year_chart" style="width: 600px;height:400px;"></div>
+	<div id="affiliation_chart" style="width: 600px;height:400px;"></div>
+	<div id="conference_chart" style="width: 600px;height:400px;"></div>
 	<?php
+	
 		$author_id = $_GET["author_id"];
 		$link = mysqli_connect("localhost:3306", 'root', '', 'test');
 		$result = mysqli_query($link, "SELECT AuthorName from authors where AuthorID='$author_id'");
@@ -28,13 +31,25 @@
 		}
 
 		$author_statistics = array();
-		$result = mysqli_fetch_all(mysqli_query($link, "SELECT b.PaperPublishYear from paper_author_affiliation a inner join Papers b on a.PaperID=b.PaperID where a.AuthorID='$author_id'"));
+		$affiliation_statistics = array();
+		$conference_statistics = array();
+		$result = mysqli_fetch_all(mysqli_query($link, "SELECT b.PaperPublishYear,a.AffiliationID,b.ConferenceID from paper_author_affiliation a inner join Papers b on a.PaperID=b.PaperID where a.AuthorID='$author_id'"));
 		foreach ($result as $paper_info) {
 			$publish_year = $paper_info[0];
 			if(array_key_exists($publish_year, $author_statistics))
 				$author_statistics[$publish_year]++;
 			else
 				$author_statistics[$publish_year]=1;
+			$affiliation = $paper_info[1];
+			if(array_key_exists($affiliation, $affiliation_statistics))
+				$affiliation_statistics[$affiliation]++;
+			else
+				$affiliation_statistics[$affiliation]=1;
+			$conference = $paper_info[2];
+			if(array_key_exists($conference, $conference_statistics))
+				$conference_statistics[$conference]++;
+			else
+				$conference_statistics[$conference]=1;
 		}
 		
 		$pagesize=10;
@@ -58,7 +73,7 @@
 				$author_1 = mysqli_fetch_all(mysqli_query($link, "SELECT AuthorName,authors.AuthorID from paper_author_affiliation  INNER JOIN authors on authors.AuthorID=paper_author_affiliation.AuthorID where PaperID='$paper_id' order by AuthorSequence ASC"));
 				foreach ($author_1 as $author) {
 					$author_id = $author[1];
-					echo "<a href='author.php?author_id=$author_id&page=".(1)."'>$author[0] </a>";
+					echo "<a href='author.php?author_id=$author_id&page=".(1)."'>$author[0]</a>; ";
 					
 				}
 				$conf_info = mysqli_fetch_array(mysqli_query($link, "SELECT ConferenceName from conferences where ConferenceID='$conf_id'"));
@@ -80,26 +95,61 @@
 	?>
 	
 	<script type="text/javascript">
-		var myChart = echarts.init(document.getElementById('paperchart'));
 		<?php
+			echo "var author_name = \"$author_name\";\n";
 			ksort($author_statistics);
 			$years=array_keys($author_statistics);
 			for ($y=$years[0]; $y<=$years[count($years)-1]; $y++) if(!array_key_exists($y,$author_statistics)) $author_statistics[$y]=0;
 			ksort($author_statistics);
 			$years=implode(", ",array_keys($author_statistics));
 			$counts=implode(", ",array_values($author_statistics));
-			echo "var years = [$years];";
-			echo "var counts = [$counts];";
-			echo "var author_name = \"$author_name\";";
-			echo "//"
+			echo "var year_name = [$years];\n";
+			echo "var year_count = [$counts];\n";
+			//echo "//";
+
+			ksort($affiliation_statistics);
+			$affiliation_names="";
+			$affiliation_datas="";
+			foreach ($affiliation_statistics as $affiliation_id=>$count)
+			{
+				if($affiliation_id=="None") $affiliation_name='None';
+				else{
+					$result=mysqli_fetch_array(mysqli_query($link, "SELECT AffiliationName from affiliations where AffiliationID='$affiliation_id'"));
+					$affiliation_name=$result['AffiliationName'];
+				}
+				$affiliation_names=$affiliation_names."'$affiliation_name', ";
+				$affiliation_datas=$affiliation_datas."{value:$count,name:'$affiliation_name'}, ";
+				// $affiliation_names=$affiliation_names."'$affiliation_id',";
+				// $affiliation_datas=$affiliation_datas."{value:$count,name:'$affiliation_id'},";
+			}
+			echo "var affiliation_name = [$affiliation_names];\n";
+			echo "var affiliation_data = [$affiliation_datas];\n";
+
+
+			ksort($conference_statistics);
+			$conference_names="";
+			$conference_datas="";
+			foreach ($conference_statistics as $conference_id=>$count)
+			{
+				$result=mysqli_fetch_array(mysqli_query($link, "SELECT ConferenceName from conferences where ConferenceID='$conference_id'"));
+				$conference_name=$result['ConferenceName'];
+				$conference_names=$conference_names."'$conference_name', ";
+				$conference_datas=$conference_datas."{value:$count,name:'$conference_name'}, ";
+			}
+			echo "var conference_name = [$conference_names];\n";
+			echo "var conference_data = [$conference_datas];\n";
+			//echo "//";
+
+
 		?>
 
+		var year_chart = echarts.init(document.getElementById('year_chart'));
 		option = {
 			title: {
-				text: 'Author Papers Statistics'
+				text: 'Author Papers Timeline Statistics'
 			},
 			legend: {
-				data: [years],
+				data: [year_name],
 				align: 'left'
 			},
 			toolbox: {
@@ -116,7 +166,7 @@
 			},
 			tooltip: {},
 			xAxis: {
-				data: years,
+				data: year_name,
 				silent: false,
 				splitLine: {
 					show: false
@@ -127,7 +177,7 @@
 			series: [{
 				name: author_name,
 				type: 'bar',
-				data: counts,
+				data: year_count,
 				animationDelay: function (idx) {
 					return idx * 10;
 				}
@@ -137,8 +187,78 @@
 				return idx * 5;
 			}
 		};
-		
-		myChart.setOption(option);
+		year_chart.setOption(option);
+
+		var affiliation_chart = echarts.init(document.getElementById('affiliation_chart'));
+		option = {
+			title : {
+				text: 'Author Affiliations Statistics',
+				x:'center'
+			},
+			tooltip : {
+				trigger: 'item',
+				formatter: "{b}<br/>{d}% ({c})"
+			},
+			legend: {
+				orient: 'vertical',
+				left: 'right',
+				data: affiliation_name
+			},
+			series : [
+				{
+					name: 'Affiliations',
+					type: 'pie',
+					radius : '55%',
+					center: ['50%', '60%'],
+					data:affiliation_data,
+					itemStyle: {
+						emphasis: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
+		};
+		affiliation_chart.setOption(option);
+
+
+		var conference_chart = echarts.init(document.getElementById('conference_chart'));
+		option = {
+			title : {
+				text: 'Author Affiliations Statistics',
+				x:'center'
+			},
+			tooltip : {
+				trigger: 'item',
+				formatter: "{b}<br/>{d}% ({c})"
+			},
+			legend: {
+				orient: 'vertical',
+				left: 'right',
+				data: conference_name
+			},
+			series : [
+				{
+					name: 'Affiliations',
+					type: 'pie',
+					radius : '55%',
+					center: ['50%', '60%'],
+					data:conference_data,
+					itemStyle: {
+						emphasis: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
+		};
+		conference_chart.setOption(option);
+
+
 	</script>
 </body>
 
